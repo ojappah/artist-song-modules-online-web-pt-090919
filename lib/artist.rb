111c6de55dc2810812a064e4f90e68b71f128c55
generous-vector-9581 <ojappah@gmail.com>
2019-10-19 17:40:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,8 +6,7 @@ class Artist
   
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-  include Memorable::InstanceMethods
-  include Paramable::InstanceMethods
+
   
   @@artists = []
 
